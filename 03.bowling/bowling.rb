ed8b5d0a1727712f7ec68aea797a5c65aba5f1e4
ystk3411 <yoshi.hama0701@gmail.com>
2026-01-02 18:33:23 +0900

    加算の処理を修正

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -17,24 +17,25 @@ if frames.count >= FRAME_TWELFTH
 
   if frames[9][0] == 10
 
-    frames[9].delete(0) if frames[9].count > FRAME_THIRD
-    frames[9] << 0 until frames[9].count == FRAME_THIRD
+    frames[9].delete(0) if frames[9].count > FRAME_LAST_THROW
+    frames[9] << 0 until frames[9].count == FRAME_LAST_THROW
   end
   frames.slice!(FRAME_ELEVENTH, FRAME_TWELFTH)
 end
 point = frames.each_with_index.sum do |frame, index|
   next frame.sum if index == 9
 
-  if frame[0] == 10 # strike
-    next frames[index + 1][0..1].sum + frame.sum unless frames[index + 1][0] == 10
-
-    result = (10 + frame.sum)
-    result += index == 8 ? frames[index + 1][1] : frames[index + 2][0]
-    next result
-
-  elsif frame.sum == 10 # spare
-    next frames[index + 1][0] + frame.sum
-  end
-  frame.sum
+  frame.sum + if frame[0] == 10 # strike
+                if frames[index + 1][0] == 10
+                  result = 10
+                  result += index == 8 ? frames[index + 1][1] : frames[index + 2][0]
+                else
+                  frames[index + 1][0..1].sum
+                end
+              elsif frame.sum == 10 # spare
+                frames[index + 1][0]
+              else
+                0
+              end
 end
 puts point
