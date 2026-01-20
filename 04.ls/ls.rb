b8b2b7489a36f33fd98f391aecd38b0a46d29454
ystk3411 <yoshi.hama0701@gmail.com>
2026-01-21 00:44:03 +0900

    メソッドの設計を修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -3,20 +3,29 @@
 
 COL_NUM = 3
 
-def transpose_files
-  files = Dir.glob('*')
-  max_size = files.map(&:size).max
-  files_sort = sort_files(files).map { |file| file.values_at(0...max_size) }
-  files_sort.transpose
+def main
+  files = fetch_files
+  files_sort = sort_files(files)
+  transpose_files(files_sort)
+end
+
+def fetch_files
+  Dir.glob('*')
 end
 
 def sort_files(files)
+  max_size = files.map(&:size).max
   m = files.size.ceildiv(COL_NUM)
   files_slice = files.each_slice(m).to_a
+  files_slice.map { |file| file.values_at(0...max_size) }
+end
+
+def transpose_files(files)
+  files.transpose
 end
 
 def output
-  transpose_files.each_with_index do |file, _index|
+  main.each_with_index do |file, _index|
     file.each do |f|
       if !f.nil?
         multiplier = 0
