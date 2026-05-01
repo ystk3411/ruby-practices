9055259eadd5ddb0ad40f7a1786fc5ebef3248b1
ystk3411 <yoshi.hama0701@gmail.com>
2026-05-01 21:03:43 +0900

    変数名とメソッド名を具体的なものに修正

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -21,41 +21,41 @@ def parse_options
 end
 
 def output(options)
-  counted_list = calc_counted_list
+  counted_file_info_list = calc_counted_file_info_list
 
-  counted_list.each do |counted_data|
-    print counted_data[:lines].rjust(8) if options[:l]
-    print counted_data[:words].rjust(8) if options[:w]
-    print counted_data[:characters].rjust(8) if options[:c]
-    print " #{counted_data[:file_name]}"
+  counted_file_info_list.each do |file_info|
+    print file_info[:lines].rjust(8) if options[:l]
+    print file_info[:words].rjust(8) if options[:w]
+    print file_info[:characters].rjust(8) if options[:c]
+    print " #{file_info[:file_name]}"
     puts
   end
 
   return unless ARGV.length > 1
 
-  total_data = count_input_data_total(options)
+  total_data = count_input_file_info_total(options)
   print total_data[:lines_total].rjust(8)
   print total_data[:words_total].rjust(8)
   print total_data[:characters_total].rjust(8)
   puts ' total'
 end
 
-def calc_counted_list
-  counted_list = []
+def calc_counted_file_info_list
+  counted_file_info_list = []
   if ARGV.empty?
-    input_data = $stdin.read
-    counted_list << count_input_data(input_data)
+    input_file_info = $stdin.read
+    counted_file_info_list << count_input_file_info(input_file_info)
   else
     ARGV.each_with_index do |file_name, index|
-      input_data = File.read(file_name)
-      counted_list << count_input_data(input_data)
-      counted_list[index][:file_name] = file_name
+      input_file_info = File.read(file_name)
+      counted_file_info_list << count_input_file_info(input_file_info)
+      counted_file_info_list[index][:file_name] = file_name
     end
   end
-  counted_list
+  counted_file_info_list
 end
 
-def count_input_data_total(options)
+def count_input_file_info_total(options)
   lines_total = 0
   words_total = 0
   characters_total = 0
@@ -69,18 +69,18 @@ def count_input_data_total(options)
     characters_total += characters
     words_total += words
   end
-  counted_input_data_total = {}
-  counted_input_data_total[:lines_total] = lines_total.to_s if options[:l]
-  counted_input_data_total[:characters_total] = characters_total.to_s if options[:w]
-  counted_input_data_total[:words_total] = words_total.to_s if options[:c]
-  counted_input_data_total
+  counted_input_file_info_total = {}
+  counted_input_file_info_total[:lines_total] = lines_total.to_s if options[:l]
+  counted_input_file_info_total[:characters_total] = characters_total.to_s if options[:w]
+  counted_input_file_info_total[:words_total] = words_total.to_s if options[:c]
+  counted_input_file_info_total
 end
 
-def count_input_data(input_data)
+def count_input_file_info(input_file_info)
   {
-    lines: input_data.count("\n").to_s,
-    words: input_data.split(/\s+/).length.to_s,
-    characters: input_data.length.to_s
+    lines: input_file_info.count("\n").to_s,
+    words: input_file_info.split(/\s+/).length.to_s,
+    characters: input_file_info.length.to_s
   }
 end
 
