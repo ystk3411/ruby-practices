2325155b7f0c5f28bdedd01b9f2f0526faf6a837
ystk3411 <yoshi.hama0701@gmail.com>
2026-05-27 00:24:00 +0900

    指摘点を修正20260527

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -5,28 +5,28 @@ require 'optparse'
 require 'etc'
 
 def main
+  options = parse_options
   file_info_list = build_file_info_list
 
   file_info_list.each do |file_info|
-    output(file_info)
+    output(file_info, options)
   end
 
   return if ARGV.length <= 1
 
   total_info = calc_total(file_info_list)
-  output(total_info)
+  output(total_info, options)
 end
 
 def build_file_info_list
-  options = parse_options
   file_info_list = []
   if ARGV.empty?
     text = $stdin.read
-    file_info_list << count_input_file_info(text, options)
+    file_info_list << count_input_file_info(text)
   else
     ARGV.each do |file_name|
       text = File.read(file_name)
-      file_info_list << count_input_file_info(text, options, file_name)
+      file_info_list << count_input_file_info(text, file_name)
     end
   end
   file_info_list
@@ -35,32 +35,33 @@ end
 def parse_options
   options = {}
   opt = OptionParser.new
-  opt.on('-l') { options[:l] = true }
-  opt.on('-w') { options[:w] = true }
-  opt.on('-c') { options[:c] = true }
+  opt.on('-l') { options[:lines] = true }
+  opt.on('-w') { options[:words] = true }
+  opt.on('-c') { options[:bytesize] = true }
   opt.parse!(ARGV)
-  options = { l: true, w: true, c: true } if options.empty?
+  options = { lines: true, words: true, bytesize: true } if options.empty?
   options
 end
 
-def count_input_file_info(text, options, file_name = nil)
+def count_input_file_info(text, file_name = nil)
   file_info = {}
-  file_info[:lines] = text.count("\n") if options[:l]
-  file_info[:words] = text.split(/\s+/).length if options[:w]
-  file_info[:characters] = text.bytesize if options[:c]
+  file_info[:lines] = text.count("\n")
+  file_info[:words] = text.split(/\s+/).length
+  file_info[:bytesize] = text.bytesize
   file_info[:file_name] = file_name if !file_name.nil?
-  file_info.compact
+  file_info
 end
 
-def output(file_info)
-  custom_order = %i[lines words characters file_name]
-  file_info_sorted = file_info.slice(*custom_order)
+def output(file_info, options)
+  custom_order = %i[lines words bytesize file_name]
+  custom_order.each do |key|
+    file_info_value = file_info[key]
+    next if file_info_value.nil?
 
-  file_info_sorted.each do |key, file_info_num|
-    if %i[file_name text].include?(key)
-      print " #{file_info_num}"
-    else
-      print file_info_num.to_s.rjust(8)
+    if %i[file_name].include?(key)
+      print " #{file_info_value}"
+    elsif options[key.to_sym]
+      print file_info_value.to_s.rjust(8)
     end
   end
   puts
@@ -70,13 +71,12 @@ def calc_total(file_info_list)
   total_num_list = {
     lines: 0,
     words: 0,
-    characters: 0,
-    file_name: ''
+    bytesize: 0,
+    file_name: 'total'
   }
 
   file_info_list.each do |file_info|
     file_info.each do |key, value|
-      total_num_list.delete_if { |key| }
       next if key == :file_name
 
       total_num_list[key] += value
@@ -84,7 +84,6 @@ def calc_total(file_info_list)
   end
 
   total_num_list.delete_if { |key| !file_info_list[0].key?(key) }
-  total_num_list[:file_name] = 'total'
   total_num_list
 end
 
